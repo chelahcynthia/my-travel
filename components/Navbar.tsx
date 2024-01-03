26bf66c415479688ed388cd252f1df4bf3e17ecb
chelahcynthia <cchelangat989@gmail.com>
2024-01-03 12:52:02 +0300

    Create a reusable button component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -17,6 +17,16 @@ const Navbar = () => {
             </Link>
             ))}
         </ul>
+
+        <div className="lg:flexCenter hidden">
+          <Button 
+          type="button"
+          title="Login"
+          
+          
+          />
+
+        </div>
     </nav>
          )
 }
