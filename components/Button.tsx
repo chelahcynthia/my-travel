e52d06b1419e41d0258525a44347d431e9c8d3bf
chelahcynthia <cchelangat989@gmail.com>
2024-01-03 12:56:48 +0300

    Create buttonprop type

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,1 +1,18 @@
-im
+import { type } from "os"
+
+
+type ButtonProps = {
+    type: 'button' | 'submit';
+    title: string;
+    icon?: string;
+    variant: string;
+    full?: boolean;
+}
+
+function Button() {
+  return (
+    <div>Button</div>
+  )
+}
+
+export default Button
