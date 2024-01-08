ffc4c60a7e99dcb8d9828a64c38d6e5886232d44
chelahcynthia <cchelangat989@gmail.com>
2024-01-08 12:13:25 +0300

    Creates hidden burger for large screens

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -27,6 +27,14 @@ const Navbar = () => {
           variant="btn_dark_green"
         />
         </div>
+        {/* hamburger menu part */}
+        <Image 
+        src="menu.svg"
+        alt="menu"
+        width={32}
+        height={32}
+        className="inline-block cursor-pointer lg:hidden"
+      />
     </nav>
          )
 }
